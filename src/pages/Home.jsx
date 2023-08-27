565bdbd7c6e70e49cea85a6d4def51ada2fe2ec3
Yuriy-St <yuriystaynov@gmail.com>
2023-08-27 15:31:18 +0300

    country list

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useFetchCountries } from 'hooks';
 
 export const Home = () => {
+  const { countires, isLoading, error } = useFetchCountries();
   return (
     <Section>
       <Container>
-        <h2>Home</h2>
+        {error && (
+          <Heading textAlign="center">Oop! Something went wrong...</Heading>
+        )}
+        {isLoading && <Loader />}
+        {countires.length > 0 && <CountryList countries={countires} />}
       </Container>
     </Section>
   );
